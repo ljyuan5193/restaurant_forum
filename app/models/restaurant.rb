74e3de759a375db95709bd721979cfdb22769599
laibuc  <ljyuan5193@gmail.com>
2017-12-12 07:57:35 +0800

    S15L2 setup for heroku deployment

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
-  validates_presence_of :name
+  validates_presence_of :name,:tel,:address,:opening_hours,:description
   mount_uploader :image, PhotoUploader
   belongs_to :category, optional: true
+  
 end
